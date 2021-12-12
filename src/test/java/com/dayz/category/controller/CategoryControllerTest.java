21463aabef5d85dad7c1d63083f3facb76d2b056
devrunner21 <devrunner21@gmail.com>
2021-12-12 14:59:01 +0900

    [test] #31 카테고리 목록 조회 Controller 통합테스트를 작성하라

--- src/test/java/com/dayz/category/controller/CategoryControllerTest.java
+++ src/test/java/com/dayz/category/controller/CategoryControllerTest.java
@@ -1,48 +1,37 @@
 package com.dayz.category.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.dayz.category.converter.CategoryConverter;
 import com.dayz.category.domain.Category;
-import com.dayz.category.dto.ReadAllCategoriesResponse;
-import com.dayz.category.service.CategoryService;
-import com.dayz.common.dto.ApiResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.dayz.category.domain.CategoryRepository;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("CategoryController 단위 테스트")
+@ActiveProfiles("test")
+@DisplayName("CategoryController 통합 테스트")
 class CategoryControllerTest {
-
-    @MockBean
-    private CategoryService categoryService;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private CategoryConverter categoryConverter;
+    private CategoryRepository categoryRepository;
 
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Test
-    @DisplayName("카테고리 목록 조회")
-    void categories_success() throws Exception {
-
-        // given
+    @BeforeEach
+    void setUp() {
         List<Category> categories = List.of(
                 Category.of(1L, "도자기"),
                 Category.of(2L, "네온싸인 공예"),
@@ -50,14 +39,20 @@ class CategoryControllerTest {
                 Category.of(4L, "요리")
         );
 
-        ReadAllCategoriesResponse readAllCategoriesResponse = categoryConverter.convertToReadAllCategoryResponse(categories);
-        given(categoryService.getAllCategoryList()).willReturn(readAllCategoriesResponse);
+        categoryRepository.saveAll(categories);
+    }
 
-        // when // then
+    @Test
+    @DisplayName("카테고리 목록 조회")
+    void categories_success() throws Exception {
         mockMvc.perform(get("/api/v1/categories"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(objectMapper.writeValueAsString(ApiResponse.<ReadAllCategoriesResponse>ok(readAllCategoriesResponse))))
                 .andExpect(status().isOk());
+    }
+
+    @AfterEach
+    void tearDown() {
+        categoryRepository.deleteAll();
     }
 
 }
