713653604cae21b6a8c3d10d3373946ee6b16789
devrunner21 <devrunner21@gmail.com>
2021-12-12 23:31:58 +0900

    [feat] #26 원데이클래스 Converter를 정의하라

---
+++ src/main/java/com/dayz/onedayclass/converter/OneDayClassConverter.java
@@ -0,0 +1,13 @@
+package com.dayz.onedayclass.converter;
+
+import com.dayz.common.util.ImageUrlUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OneDayClassConverter {
+
+    private final ImageUrlUtil imageUrlUtil;
+
+}
