af093b87d21446aa5c18265174758e5e80c78031
devrunner21 <devrunner21@gmail.com>
2021-12-12 23:22:18 +0900

    [feat] #26 요청받은 지역정보 정합성 체크를 위한 Repository 메서드를 정의하라

--- src/main/java/com/dayz/member/domain/AddressRepository.java
+++ src/main/java/com/dayz/member/domain/AddressRepository.java
@@ -1,7 +1,10 @@
 package com.dayz.member.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    Optional<Address> findAddressByCityIdAndRegionIdAndUseFlagIsTrue(Long cityId, Long regionId);
 
 }
