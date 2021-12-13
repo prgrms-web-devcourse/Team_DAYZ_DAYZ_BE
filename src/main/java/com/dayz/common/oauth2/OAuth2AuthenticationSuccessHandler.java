742f293e640bb145862a61cbefe34c6f66a36645
devrunner21 <devrunner21@gmail.com>
2021-12-13 13:12:27 +0900

    [feat] #23 로그인 후 데이터를 QueryString으로 내려주도록 변경하라

--- src/main/java/com/dayz/common/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/dayz/common/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -7,9 +7,11 @@ import com.dayz.member.service.MemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -18,9 +20,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    public final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private final Jwt jwt;
@@ -33,7 +37,8 @@ public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthent
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws ServletException, IOException {
         /**
          * JWT
          */
@@ -43,17 +48,58 @@ public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthent
             String registrationId = oauth2Token.getAuthorizedClientRegistrationId();
 
             Member member = processUserOAuth2UserJoin(principal, registrationId);
-            String loginSuccessJson = generateLoginSuccessJson(member);
-            response.setContentType("application/json;charset=UTF-8");
-            response.setContentLength(loginSuccessJson.getBytes(StandardCharsets.UTF_8).length);
-            response.getWriter().write(loginSuccessJson);
+            String token = generateToken(member);
+
+            String targetUrl = determineTargetUrl(request, response, token, member);
+            getRedirectStrategy().sendRedirect(request, response, targetUrl);
         } else {
             super.onAuthenticationSuccess(request, response, authentication);
         }
+
     }
 
     private Member processUserOAuth2UserJoin(OAuth2User oAuth2User, String registrationId) {
         return memberService.join(oAuth2User, registrationId);
+    }
+
+    private String generateToken(Member member) {
+        return jwt
+                .sign(Jwt.Claims.from(member.getId(), member.getProviderId(), member.getUsername(), new String[]{member.getPermission().getName()}));
+    }
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, String jwtToken, Member member) {
+
+        Optional<String> redirectUri = getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME).map(Cookie::getValue);
+
+//        if(redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+//            throw new IllegalArgumentException("Sorry! We've got an Unauthorized Redirect URI and can't proceed with the authentication");
+//        }
+
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+        String auth = member.getPermission().getName();
+        boolean addressInfoFlag = !Objects.isNull(member.getAddress());
+        boolean atelierInfoFlag = !Objects.isNull(member.getAtelier());
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", jwtToken)
+                .queryParam("addressInfoFlag", addressInfoFlag)
+                .queryParam("atelierInfoFlag", atelierInfoFlag)
+                .queryParam("auth", auth)
+                .build().toUriString();
+    }
+
+    public Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+        return Optional.empty();
     }
 
     private String generateLoginSuccessJson(Member member) throws JsonProcessingException {
@@ -63,10 +109,6 @@ public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthent
         String responseString = mapper.writeValueAsString(response);
         log.debug("Jwt({}) created for oauth2 login user {}", token, member.getUsername());
         return responseString;
-    }
-
-    private String generateToken(Member member) {
-        return jwt.sign(Jwt.Claims.from(member.getId(), member.getProviderId(), member.getUsername(), new String[]{member.getPermission().getName()}));
     }
 
 }
