a7246881ce7c701d85a26c4677bbb30fd5f78b08
pinoa1228 <pinoa1228@gmail.com>
2021-12-14 16:03:24 +0900

    [feat] #35 ReviewRepository 구현에 따른 Querydsl 설정

---
+++ src/main/java/com/dayz/review/domain/CustomRepository.java
@@ -0,0 +1,14 @@
+package com.dayz.review.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+public interface CustomRepository {
+
+    Page<Review> findAllByMemberId(Long memberId, Pageable pageable);
+
+    Page<Review> findAllByAtelierId(@Param("id") Long id, Pageable pageable);
+}
