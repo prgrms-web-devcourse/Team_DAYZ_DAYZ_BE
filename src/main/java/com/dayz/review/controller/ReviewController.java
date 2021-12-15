9e392ee29afa8796fe70ab486b7db5c26c49f6d9
pinoa1228 <pinoa1228@gmail.com>
2021-12-15 23:02:45 +0900

    [feat] #36 리뷰 작성을 위한 컨트롤러와 컨트롤러 테스트 구현

--- src/main/java/com/dayz/review/controller/ReviewController.java
+++ src/main/java/com/dayz/review/controller/ReviewController.java
@@ -5,11 +5,16 @@ import com.dayz.common.dto.ApiResponse;
 import com.dayz.common.dto.CustomPageRequest;
 import com.dayz.common.dto.CustomPageResponse;
 import com.dayz.member.domain.Member;
+import com.dayz.review.dto.SaveReviewRequest;
 import com.dayz.review.service.ReviewService;
+import java.util.HashMap;
+import java.util.Map;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +36,10 @@ public class ReviewController {
     public ApiResponse<CustomPageResponse> getAtelierReviews(
         @PathVariable("atelierId") Long atelierId, @RequestBody CustomPageRequest pageRequest) {
         return ApiResponse.ok(reviewService.getAllAtelierReviews(pageRequest, atelierId));
+    }
+
+    @PostMapping(value = "/reviews", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<Map<String,Long>> saveReviews(@LoginMember Member member,@Valid @RequestBody SaveReviewRequest saveReviewRequest) {
+        return ApiResponse.ok(Map.of("reviewId",reviewService.saveReview(saveReviewRequest, member)));
     }
 }
