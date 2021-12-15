9548e3044360e1b4c76d00d448b256a0d188557e
devrunner21 <devrunner21@gmail.com>
2021-12-16 00:56:50 +0900

    [refactor] #9 정렬기능을 추가하라

--- src/main/java/com/dayz/atelier/domain/QAtelierRepositoryImpl.java
+++ src/main/java/com/dayz/atelier/domain/QAtelierRepositoryImpl.java
@@ -4,12 +4,16 @@ import static com.dayz.atelier.domain.QAtelier.atelier;
 import static com.dayz.member.domain.QAddress.address;
 import static com.dayz.member.domain.QMember.member;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 
@@ -24,15 +28,22 @@ public class QAtelierRepositoryImpl implements QAtelierRepository {
             @Param("regionId") Long regionId,
             Pageable pageRequest
     ) {
-        List<Atelier> content = jpaQueryFactory.selectFrom(atelier)
+        JPAQuery<Atelier> contentQuery = jpaQueryFactory.selectFrom(atelier)
                 .innerJoin(atelier.member, member).fetchJoin()
                 .innerJoin(atelier.address, address).fetchJoin()
                 .where(address.cityId.eq(cityId)
                         .and(atelier.address.regionId.eq(regionId))
                         .and(atelier.address.useFlag.eq(true))
                 ).offset(pageRequest.getOffset())
-                .limit(pageRequest.getPageSize())
-                .fetch();
+                .limit(pageRequest.getPageSize());
+
+        for (Sort.Order o : pageRequest.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(atelier.getType(), atelier.getMetadata());
+            contentQuery.orderBy(new OrderSpecifier<>(o.isAscending() ? Order.ASC : Order.DESC,
+                    pathBuilder.get(o.getProperty())));
+        }
+
+        List<Atelier> content = contentQuery.fetch();
 
         JPAQuery<Atelier> countQuery = jpaQueryFactory.selectFrom(atelier)
                 .innerJoin(atelier.address, address)
