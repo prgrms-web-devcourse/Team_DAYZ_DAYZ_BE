e23ec789b54d74db573106890cc606fa340e2bdd
devrunner21 <devrunner21@gmail.com>
2021-12-15 20:17:57 +0900

    [feat] #9 공방목록 조회용 Convert 메서드를 구현하라

--- src/main/java/com/dayz/atelier/converter/AtelierConverter.java
+++ src/main/java/com/dayz/atelier/converter/AtelierConverter.java
@@ -1,6 +1,10 @@
 package com.dayz.atelier.converter;
 
+import com.dayz.atelier.domain.Atelier;
+import com.dayz.atelier.dto.ReadAteliersResult;
 import com.dayz.atelier.dto.SaveAtelierResponse;
+import com.dayz.common.util.ImageUrlUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +12,15 @@ public class AtelierConverter {
 
     public SaveAtelierResponse convertToSaveAtelierResponse(Long atelierId, String token) {
         return SaveAtelierResponse.of(atelierId, token);
+    }
+
+    public ReadAteliersResult convertToReadAteliersResult(Atelier atelier) {
+        return ReadAteliersResult.of(
+                atelier.getId(),
+                atelier.getName(),
+                atelier.getMember().getProfileImageUrl(),
+                atelier.getIntro()
+        );
     }
 
 }
