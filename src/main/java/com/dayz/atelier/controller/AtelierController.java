5a6f56de970e336cd6ee016d8e324eaee6eae27e
devrunner21 <devrunner21@gmail.com>
2021-12-16 02:28:38 +0900

    [feat] #11 공방 상세조회 API를 구현하라

--- src/main/java/com/dayz/atelier/controller/AtelierController.java
+++ src/main/java/com/dayz/atelier/controller/AtelierController.java
@@ -1,5 +1,6 @@
 package com.dayz.atelier.controller;
 
+import com.dayz.atelier.dto.ReadAtelierDetailResponse;
 import com.dayz.atelier.dto.SaveAtelierRequest;
 import com.dayz.atelier.dto.SaveAtelierResponse;
 import com.dayz.atelier.service.AtelierService;
@@ -10,6 +11,8 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AtelierController {
 
     private final AtelierService atelierService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/{atelierId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<ReadAtelierDetailResponse> getAtelierDetail(@PathVariable("atelierId") Long atelierId) {
+
+        ReadAtelierDetailResponse response = atelierService.getAtelierDetail(atelierId);
+
+        return ApiResponse.<ReadAtelierDetailResponse>ok(response);
+    }
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
