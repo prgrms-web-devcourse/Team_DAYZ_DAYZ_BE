c8932f947fcaad4dcf2ea701444f9b19ac6d6de4
devrunner21 <devrunner21@gmail.com>
2021-12-15 20:18:33 +0900

    [feat] #9 공방 목록 조회 API를 구현하라

--- src/main/java/com/dayz/atelier/controller/AtelierController.java
+++ src/main/java/com/dayz/atelier/controller/AtelierController.java
@@ -1,15 +1,22 @@
 package com.dayz.atelier.controller;
 
+import com.dayz.atelier.domain.Atelier;
+import com.dayz.atelier.dto.ReadAteliersResult;
 import com.dayz.atelier.dto.SaveAtelierRequest;
 import com.dayz.atelier.dto.SaveAtelierResponse;
 import com.dayz.atelier.service.AtelierService;
 import com.dayz.common.aop.LoginMember;
 import com.dayz.common.dto.ApiResponse;
+import com.dayz.common.dto.CustomPageRequest;
+import com.dayz.common.dto.CustomPageResponse;
+import com.dayz.common.jwt.JwtAuthentication;
 import com.dayz.member.domain.Member;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +31,23 @@ public class AtelierController {
     private final AtelierService atelierService;
 
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ApiResponse<SaveAtelierResponse> saveAtelier(@LoginMember Member member, @Valid @RequestBody SaveAtelierRequest request) {
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<CustomPageResponse<ReadAteliersResult>> getAteliers(
+            @AuthenticationPrincipal JwtAuthentication authentication,
+            @Valid @RequestBody CustomPageRequest request) {
+        CustomPageResponse<ReadAteliersResult> response = atelierService.getAteliers(authentication.getId(), request.convertToPageRequest(Atelier.class));
 
-        SaveAtelierResponse response = atelierService.savaAtelierInfo(member.getId(), request);
+        return ApiResponse.<CustomPageResponse<ReadAteliersResult>>ok(response);
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ApiResponse<SaveAtelierResponse> saveAtelier(
+            @AuthenticationPrincipal JwtAuthentication authentication,
+            @Valid @RequestBody SaveAtelierRequest request) {
+
+        SaveAtelierResponse response = atelierService.savaAtelierInfo(authentication.getId(), request);
 
         return ApiResponse.<SaveAtelierResponse>ok(response);
     }
