b6f3498f0684fa6c6d2a163c69cc201ff48a5b31
pinoa1228 <pinoa1228@gmail.com>
2021-12-17 11:51:11 +0900

    [feat] #32 원데이 클래스 이름으로 검색을 위한 repository 구현

--- src/main/java/com/dayz/onedayclass/domain/QOneDayClassRepository.java
+++ src/main/java/com/dayz/onedayclass/domain/QOneDayClassRepository.java
@@ -2,14 +2,15 @@ package com.dayz.onedayclass.domain;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 
 public interface QOneDayClassRepository {
 
     Page<OneDayClass> findOneDayClassByCategoryId(
-            Long categoryId,
-            Long cityId,
-            Long regionId,
-            Pageable pageRequest);
+        Long categoryId,
+        Long cityId,
+        Long regionId,
+        Pageable pageRequest);
 
+    Page<OneDayClass> searchOneDayClass(Long cityId, Long regionId, String keyWord,
+        Pageable pageable);
 }
--- src/main/java/com/dayz/onedayclass/domain/QOneDayClassRepositoryImpl.java
+++ src/main/java/com/dayz/onedayclass/domain/QOneDayClassRepositoryImpl.java
@@ -4,6 +4,8 @@ import static com.dayz.atelier.domain.QAtelier.atelier;
 import static com.dayz.category.domain.QCategory.category;
 import static com.dayz.member.domain.QAddress.address;
 import static com.dayz.onedayclass.domain.QOneDayClass.oneDayClass;
+import static com.dayz.onedayclass.domain.QOneDayClassImage.oneDayClassImage;
+
 
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
@@ -24,38 +26,97 @@ public class QOneDayClassRepositoryImpl implements QOneDayClassRepository {
 
     @Override
     public Page<OneDayClass> findOneDayClassByCategoryId(
-            Long categoryId,
-            Long cityId,
-            Long regionId,
-            Pageable pageRequest) {
+        Long categoryId,
+        Long cityId,
+        Long regionId,
+        Pageable pageRequest) {
 
         JPAQuery<OneDayClass> contentQuery = jpaQueryFactory.selectFrom(oneDayClass)
-                .innerJoin(oneDayClass.atelier, atelier).fetchJoin()
-                .innerJoin(atelier.address, address).fetchJoin()
-                .innerJoin(oneDayClass.category, category).fetchJoin()
-                .where(oneDayClass.category.id.eq(categoryId)
-                        .and(atelier.address.cityId.eq(cityId))
-                        .and(atelier.address.regionId.eq(regionId)))
-                .offset(pageRequest.getOffset())
-                .limit(pageRequest.getPageSize());
+            .innerJoin(oneDayClass.atelier, atelier).fetchJoin()
+            .innerJoin(atelier.address, address).fetchJoin()
+            .innerJoin(oneDayClass.category, category).fetchJoin()
+            .where(oneDayClass.category.id.eq(categoryId)
+                .and(atelier.address.cityId.eq(cityId))
+                .and(atelier.address.regionId.eq(regionId)))
+            .offset(pageRequest.getOffset())
+            .limit(pageRequest.getPageSize());
 
         for (Sort.Order o : pageRequest.getSort()) {
-            PathBuilder pathBuilder = new PathBuilder(oneDayClass.getType(), oneDayClass.getMetadata());
+            PathBuilder pathBuilder = new PathBuilder(oneDayClass.getType(),
+                oneDayClass.getMetadata());
             contentQuery.orderBy(new OrderSpecifier<>(o.isAscending() ? Order.ASC : Order.DESC,
-                    pathBuilder.get(o.getProperty())));
+                pathBuilder.get(o.getProperty())));
         }
 
         List<OneDayClass> content = contentQuery.fetch();
 
         JPAQuery<OneDayClass> countQuery = jpaQueryFactory.selectFrom(oneDayClass)
-                .innerJoin(oneDayClass.atelier, atelier).fetchJoin()
-                .innerJoin(oneDayClass.category, category).fetchJoin()
-                .innerJoin(oneDayClass.atelier.address, address).fetchJoin()
-                .where(oneDayClass.category.id.eq(categoryId)
-                        .and(oneDayClass.atelier.address.cityId.eq(cityId))
-                        .and(oneDayClass.atelier.address.regionId.eq(regionId)));
+            .innerJoin(oneDayClass.atelier, atelier).fetchJoin()
+            .innerJoin(oneDayClass.category, category).fetchJoin()
+            .innerJoin(oneDayClass.atelier.address, address).fetchJoin()
+            .where(oneDayClass.category.id.eq(categoryId)
+                .and(oneDayClass.atelier.address.cityId.eq(cityId))
+                .and(oneDayClass.atelier.address.regionId.eq(regionId)));
 
         return PageableExecutionUtils.getPage(content, pageRequest, countQuery::fetchCount);
     }
+
+    @Override
+    public Page<OneDayClass> searchOneDayClass(Long cityId, Long regionId, String keyWord,Pageable pageRequest) {
+
+        JPAQuery<OneDayClass> results = jpaQueryFactory
+            .selectFrom(oneDayClass)
+            .innerJoin(oneDayClass.atelier, atelier).fetchJoin()
+            .leftJoin(oneDayClass.oneDayClassImages, oneDayClassImage).fetchJoin()
+            .where(oneDayClass.useFlag.eq(true)
+                    .and(oneDayClass.name.contains(keyWord))
+                    .and(atelier.address.cityId.eq(cityId))
+                    .and(atelier.address.regionId.eq(regionId))
+            )
+            .offset(pageRequest.getOffset())
+            .limit(pageRequest.getPageSize());
+
+        for (Sort.Order o : pageRequest.getSort()) {
+            PathBuilder pathBuilder = new PathBuilder(oneDayClass.getType(),
+                oneDayClass.getMetadata());
+            results.orderBy(new OrderSpecifier<>(o.isAscending() ? Order.ASC : Order.DESC,
+                pathBuilder.get(o.getProperty())));
+        }
+
+        return PageableExecutionUtils.getPage(results.fetch(), pageRequest, results::fetchCount);
+    }
+
+//    @Override
+//    public Page<SearchOneDayClassResponse> searchOneDayClass(Long cityId, Long regionId,
+//        String keyWord,
+//        Pageable pageRequest) {
+//
+//        JPAQuery<SearchOneDayClassResponse> results = jpaQueryFactory
+//            .select(new QSearchOneDayClassResult(
+//                oneDayClass.id,
+//                oneDayClass.name,
+//                oneDayClass.intro,
+//                oneDayClass.oneDayClassImages))
+//            .from(oneDayClass)
+//            .leftJoin(oneDayClass.atelier, atelier)
+//            .leftJoin(oneDayClassImage).on(oneDayClass.id.eq(oneDayClassImage.oneDayClass.id))
+//            .where(oneDayClass.useFlag.eq(true)
+//                .and(oneDayClassImage.sequence.eq(1))
+//                .and(oneDayClass.name.contains(keyWord))
+//                .and(atelier.address.cityId.eq(cityId))
+//                .and(atelier.address.regionId.eq(regionId))
+//            )
+//            .offset(pageRequest.getOffset())
+//            .limit(pageRequest.getPageSize());
+//
+//        for (Sort.Order o : pageRequest.getSort()) {
+//            PathBuilder pathBuilder = new PathBuilder(oneDayClass.getType(),
+//                oneDayClass.getMetadata());
+//            results.orderBy(new OrderSpecifier<>(o.isAscending() ? Order.ASC : Order.DESC,
+//                pathBuilder.get(o.getProperty())));
+//        }
+//
+//        return PageableExecutionUtils.getPage(results.fetch(), pageRequest, results::fetchCount);
+//    }
 
 }
