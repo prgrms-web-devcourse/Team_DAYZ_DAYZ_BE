362359e529209351b41e6d30be7d61fe45747795
devrunner21 <devrunner21@gmail.com>
2021-12-17 11:35:25 +0900

    [feat] #25 금주의 인기 클래스 조회를 위한 Response 객체를 정의하라

---
+++ src/main/java/com/dayz/onedayclass/dto/ReadPopularOneDayClassesResponse.java
@@ -0,0 +1,49 @@
+package com.dayz.onedayclass.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReadPopularOneDayClassesResponse {
+
+    private List<OneDayClassResult> oneDayClasses = new ArrayList<>();
+
+    public static ReadPopularOneDayClassesResponse of(List<OneDayClassResult> oneDayClasses) {
+        ReadPopularOneDayClassesResponse readPopularOneDayClassesResponse = new ReadPopularOneDayClassesResponse();
+        readPopularOneDayClassesResponse.setOneDayClasses(oneDayClasses);
+
+        return readPopularOneDayClassesResponse;
+    }
+
+    @Getter
+    @Setter(AccessLevel.PRIVATE)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class OneDayClassResult {
+
+        private Long oneDayClassId;
+
+        private String name;
+
+        private String intro;
+
+        private String imageUrl;
+
+        public static OneDayClassResult of(Long oneDayClassId, String name, String intro, String imageUrl) {
+            OneDayClassResult oneDayClassResult = new OneDayClassResult();
+            oneDayClassResult.setOneDayClassId(oneDayClassId);
+            oneDayClassResult.setName(name);
+            oneDayClassResult.setIntro(intro);
+            oneDayClassResult.setImageUrl(imageUrl);
+
+            return oneDayClassResult;
+        }
+
+    }
+
+}
