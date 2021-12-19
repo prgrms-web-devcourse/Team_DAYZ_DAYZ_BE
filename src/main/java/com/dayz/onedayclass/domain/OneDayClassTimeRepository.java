71256c8261133d2f3fd2490d7ede647a996c7b53
devrunner21 <devrunner21@gmail.com>
2021-12-19 20:17:11 +0900

    [fix] 원데이 클래스 시간 조회 시 Reservation과의 Join을 Outer Join으로 변경

--- src/main/java/com/dayz/onedayclass/domain/OneDayClassTimeRepository.java
+++ src/main/java/com/dayz/onedayclass/domain/OneDayClassTimeRepository.java
@@ -9,14 +9,14 @@ import org.springframework.data.repository.query.Param;
 public interface OneDayClassTimeRepository extends JpaRepository<OneDayClassTime, Long> {
 
     final String SQL_findOneDayClassTimesByDate =
-            "SELECT ot.onedayclass_time_id AS 'classTimeId' , o.max_people_number AS 'maxPeopleNumber', tmp.current_people_number AS 'currentPeopleNumber', ot.start_time AS 'startTime', ot.end_time AS 'endTime', ot.status AS 'status'"
+            "SELECT ot.onedayclass_time_id AS 'classTimeId' , o.max_people_number AS 'maxPeopleNumber', IFNULL(tmp.current_people_number,0) AS 'currentPeopleNumber', ot.start_time AS 'startTime', ot.end_time AS 'endTime', ot.status AS 'status'"
             + "FROM onedayclass_time ot "
             + " INNER JOIN onedayclass o "
             + "    ON ot.onedayclass_id = o.onedayclass_id "
             + " INNER JOIN  ("
             + "     SELECT ot.onedayclass_time_id AS time_id, ot.class_date AS class_date, sum(r.people_number) AS current_people_number"
             + "     FROM onedayclass_time ot "
-            + "     INNER JOIN reservation r "
+            + "     LEFT OUTER JOIN reservation r "
             + "         ON ot.onedayclass_time_id = r.onedayclass_time_id"
             + "     GROUP BY ot.onedayclass_time_id "
             + "     HAVING class_date = :date"
