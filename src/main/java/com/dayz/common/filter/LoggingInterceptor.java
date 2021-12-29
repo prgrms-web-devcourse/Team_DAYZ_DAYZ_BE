ffd942d195100c734081e046c41557a69e521a39
devrunner21 <devrunner21@gmail.com>
2021-12-29 15:26:14 +0900

    [chore] #1 Request, Response 로깅 인터셉터를 정의하라

---
+++ src/main/java/com/dayz/common/filter/LoggingInterceptor.java
@@ -0,0 +1,39 @@
+package com.dayz.common.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        if (request.getClass().getName().contains("SecurityContextHolderAwareRequestWrapper"))
+            return;
+
+        final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+        final ContentCachingResponseWrapper cachingResponse = (ContentCachingResponseWrapper) response;
+        if (cachingRequest.getContentType() != null && cachingRequest.getContentType().contains("application/json")) {
+            if (cachingRequest.getContentAsByteArray() != null && cachingRequest.getContentAsByteArray().length != 0){
+                log.info("Request Body : {}", objectMapper.readTree(cachingRequest.getContentAsByteArray()));
+            }
+        }
+
+        if (cachingResponse.getContentType() != null && cachingResponse.getContentType().contains("application/json")) {
+            if (cachingResponse.getContentAsByteArray() != null && cachingResponse.getContentAsByteArray().length != 0) {
+                log.info("Response Body : {}", objectMapper.readTree(cachingResponse.getContentAsByteArray()));
+            }
+        }
+    }
+}
+
--- src/main/java/com/dayz/config/WebMvcConfig.java
+++ src/main/java/com/dayz/config/WebMvcConfig.java
@@ -1,12 +1,14 @@
 package com.dayz.config;
 
 import com.dayz.common.aop.JwtAuthenticationArgumentResolver;
+import com.dayz.common.filter.LoggingInterceptor;
 import com.dayz.member.service.MemberService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -17,14 +19,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final JwtAuthenticationArgumentResolver addArgumentResolvers;
 
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Bean
+    public JwtAuthenticationArgumentResolver authenticationArgumentResolver() {
+        return new JwtAuthenticationArgumentResolver(memberService);
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(addArgumentResolvers);
     }
 
-    @Bean
-    public JwtAuthenticationArgumentResolver authenticationArgumentResolver() {
-        return new JwtAuthenticationArgumentResolver(memberService);
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor) .addPathPatterns("/**") .excludePathPatterns("/vendor/**", "/css/*", "/img/*");
     }
 
 }
